6f581d3191a88e23b7f3052dabd6ccf31517dbcd
javideas <infojavideas@gmail.com>
2023-01-19 16:56:11 +0200

    basic setup

---
+++ src/components/Testimonials.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Testimonial(){
+    return (
+        <div className='container-testimonial'>
+            <img className='img-testimonial'
+            src={require('../img/image1.jpeg')}
+            alt='img 1'
+            />
+        </div>
+    );
+}
